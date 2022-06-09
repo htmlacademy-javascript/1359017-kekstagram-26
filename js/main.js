587c8ad4d9b3215c19306ee149152ab276857853
Ekaterina <ezakhvataeva@gmail.com>
2022-06-10 00:52:36 +0200

    Добавляет функции

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function getRandom(min, max) {
+  if (min >= 0 && max > min){
+    return Math.floor(Math.random() * (max-min)+min);
+  }
+}
+getRandom(0, 10);
+
+function checkString (string, long){
+  if (string.length > long){
+    return false;
+  }
+  return true;
+}
